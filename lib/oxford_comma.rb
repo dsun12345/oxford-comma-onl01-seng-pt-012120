b7159c7901a5b4c09bd24acc2426cd85eef7bbad
Daniel Sun <danielsun12345@gmail.com>
2020-01-29 07:32:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  case array.size 
+  when 1 
+    array.join
+  when 2 
+    string = array.join(" and ")
+  else 
+    string = String.new 
+    array.each_with_index do |item,index|
+      string += "and " + item if index == array.size - 1 
+    break if index == array.size - 1 
+    string += item  + ", "
+  end 
+    return string 
+  end
 end
