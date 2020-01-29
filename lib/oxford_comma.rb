265c3b8f93fefac282dff0a347d7ae53c45bc34f
Daniel Sun <danielsun12345@gmail.com>
2020-01-29 07:26:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,6 +8,6 @@ def oxford_comma(array)
     string = string.new 
     array.each_with_index do |item,index|
       string += "and " + item if index == array.size - 1 
-    break if index 
+    break if index == array.size 
   end
 end
