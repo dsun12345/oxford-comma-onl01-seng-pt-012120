cd3296d2b92a2ba588e6688d59794b9d48e42ab5
Daniel Sun <danielsun12345@gmail.com>
2020-01-29 07:12:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,9 @@ def oxford_comma(array)
   elsif array.size == 3
     array[0] + ', ' + array[1] + ', ' + 'and ' + array [2]
   elsif array.size > 3
-   arrays
+   arrays.join (', ' << ' and ')
   
   else
-    array.join
+    array.join()
   end
 end
